fe14912affd434f993f791f985842411de03948a
marinebox <sh.25.tgt@gmail.com>
2021-10-25 22:20:51 +0900

    add a feature to close designate URL tabs

---
+++ popup.js
@@ -0,0 +1,24 @@
+document.getElementById('normal_action').addEventListener('click', () => {
+    chrome.tabs.query({}, tabs => {
+        tabs.map((currentTab, index) => {
+            tabs.slice(index).map(targetTab => {
+                if(currentTab.id === targetTab.id) return;
+                else if(currentTab.url === targetTab.url){
+                    chrome.tabs.remove(targetTab.id);
+                }
+                else return;
+            });
+        });
+    });
+});
+
+document.getElementById('designate_delete').addEventListener('click', () => {
+    const designatedURL = document.getElementById('designate').value;
+    chrome.tabs.query({}, tabs => {
+        tabs.map((currentTab) => {
+            if(currentTab.url.match(designatedURL)){
+                chrome.tabs.remove(currentTab.id)
+            }
+        })
+    })
+});
