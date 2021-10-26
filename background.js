0c4395180b19a4f6bec77ebf95d0be155e10c93a
penicillin0 <ayahitonakamura@gmail.com>
2021-10-27 00:03:58 +0900

    background tabcount

---
+++ background.js
@@ -0,0 +1,20 @@
+function tabCount() {
+  const tabUrlCounter = {};
+
+  chrome.tabs.query({}, tabs => {
+    
+    tabs.forEach(tab => {
+      const url = tab.url;
+      tabUrlCounter[url] = (tabUrlCounter[url] || 0) + 1;
+    });
+
+    let deplicatedTabNum = 0;
+    for (const url in tabUrlCounter) {
+      deplicatedTabNum += tabUrlCounter[url] - 1;
+    };
+    
+    chrome.action.setBadgeText({ text: deplicatedTabNum.toString() });
+  });
+}
+
+setInterval(tabCount, 1000);
