cf744d59648544218f90b0172243083f90beff2d
penicillin0 <ayahitonakamura@gmail.com>
2021-10-28 04:56:04 +0900

    format with prettier

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 function tabCount() {
   const tabUrlCounter = {};
 
-  chrome.tabs.query({}, tabs => {
-    
-    tabs.forEach(tab => {
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
       const url = tab.url;
       tabUrlCounter[url] = (tabUrlCounter[url] || 0) + 1;
     });
@@ -11,9 +10,11 @@ function tabCount() {
     let deplicatedTabNum = 0;
     for (const url in tabUrlCounter) {
       deplicatedTabNum += tabUrlCounter[url] - 1;
-    };
+    }
 
-    chrome.action.setBadgeText({ text: deplicatedTabNum > 0 ? deplicatedTabNum.toString() : '' });
+    chrome.action.setBadgeText({
+      text: deplicatedTabNum > 0 ? deplicatedTabNum.toString() : "",
+    });
   });
 }
 
