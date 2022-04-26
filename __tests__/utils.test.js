045d00d8ee80d10c96bd0bc04f671e6242cf15bf
penicillin0 <ayahitonakamura@gmail.com>
2022-04-26 20:21:43 +0900

    add test

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -12,4 +12,40 @@ describe('test_filterTabsWhichIsNotInWhiteList', () => {
     const filteredTabs = filterTabsWhichIsNotInWhiteList(tabs, whiteList);
     expect(filteredTabs).toStrictEqual([]);
   });
+
+  test('ホワイトリストに入っていないドメインは除かれない', () => {
+    const whiteList = ['example.com'];
+    const tabs = [
+      {
+        url: 'https://google.com/hoge'
+      }
+    ];
+
+    const filteredTabs = filterTabsWhichIsNotInWhiteList(tabs, whiteList);
+    expect(filteredTabs).toStrictEqual(tabs);
+  });
+
+  test('ホワイトリストに入っているURLは除かれる', () => {
+    const whiteList = ['https://example.com/'];
+    const tabs = [
+      {
+        url: 'https://example.com'
+      }
+    ];
+
+    const filteredTabs = filterTabsWhichIsNotInWhiteList(tabs, whiteList);
+    expect(filteredTabs).toStrictEqual([]);
+  });
+
+  test('ホワイトリストに入っていないURLは除かれない', () => {
+    const whiteList = [];
+    const tabs = [
+      {
+        url: 'https://example.com'
+      }
+    ];
+
+    const filteredTabs = filterTabsWhichIsNotInWhiteList(tabs, whiteList);
+    expect(filteredTabs).toStrictEqual(tabs);
+  });
 });
